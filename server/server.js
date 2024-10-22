49932427fae4917db1ede262af662c91d1842d8b
Harsimrat Singh <harsimrat11.singh@gmail.com>
2024-10-22 09:38:22 +0530

    Creating basic server and subfiles

--- server/server.js
+++ server/server.js
@@ -1,1 +1,21 @@
-console.log("TEST")
+const express=require("express");
+const connectDb=require("./config/dbConnect");
+const errorHandler=require("./middlewares/errorHandler");
+const cors=require("cors");
+
+connectDb();
+const app=express();
+const port=processor.env.port||5000;
+
+app.use(express.json());
+app.use(cors());
+
+
+//ROUTES BELOW
+app.get('/',(req,res)=>{
+    res.send("working");
+});
+//APP CONFIG START
+app.listen(port,()=>{
+    console.log('Server running on port https://localhost:${port}')
+})
